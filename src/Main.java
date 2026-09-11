c3b7c046d7e6409566c78c37f8f7c70963296498
blckryun <g4brielsugui@gmail.com>
2026-09-10 21:10:21 -0300

    adicao de menu interativo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javax.xml.transform.Source;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ContaBancaria conta1 = new ContaBancaria("Gabriel", "1", 2000); //conta1
@@ -12,7 +15,7 @@ public class Main {
             boolean sacou = cc.sacar(700);
             System.out.println("Saldo Atualiazado " + sacou);
             System.out.println("Saldo Atualiazado em R$ " + cc.getSaldo() + " Com Cheque Especial ");
-        } catch (SaldoInsuficienteException e){
+        } catch (SaldoInsuficienteException e) {
             System.out.println(e.getMessage());
         }
 
@@ -37,8 +40,54 @@ public class Main {
             conta2.sacar(700);
             conta2.exibirHistorico();
         } catch (ContaNaoEncontradaException | SaldoInsuficienteException exception) {
+            System.out.println("Erro nos Testes da Conta2 : " + exception.getMessage());
+
         }
 
+        Scanner scanner = new Scanner(System.in);
 
+        int opcao1 = -1;
+        while (opcao1 != 0) {
+            System.out.println("0 - Sair, 1 - Trasferencia, 2 - Sacar, 3 - Consultar Saldo.");
+            opcao1 = scanner.nextInt();
+
+            switch (opcao1) {
+                case 1:
+                    System.out.println("Digite o N* da Conta Destinada");
+                    String numdestino = scanner.next();
+
+                    System.out.println("Digite o Valor de Transferencia");
+                    double valor = scanner.nextDouble();
+
+                    try {
+                        ContaBancaria contaDestino = dao.buscarporNumero(numdestino);
+                        conta1.transferir(valor, contaDestino);
+                        System.out.println("Transferencia Realizada com Sucesso!");
+                    } catch (ContaNaoEncontradaException | SaldoInsuficienteException exception) {
+                        System.out.println("Erro : " + exception.getMessage());
+                    }
+                    break;
+                case 2:
+                    System.out.println("Insira o Valor para Saque");
+                    double valorSaque = scanner.nextDouble();
+
+                    try {
+                        conta1.sacar(valorSaque);
+                        System.out.println("Saque Realizado com Sucesso!");
+
+                    } catch (SaldoInsuficienteException e) {
+                        System.out.println("Erro : " + e.getMessage());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saldo Atual : " + conta1.getSaldo());
+                    break;
+                case 0:
+                    System.out.println("Sistema Encerrado!");
+                default:
+                    System.out.println("Opcao Invalida! Tente Novamente");
+                    break;
+            }
+        }
     }
 }
